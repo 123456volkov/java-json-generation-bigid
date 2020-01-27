bb3f2e1ffef56560ee3ae326520e329f39f28be0
alexandervolkov1211 <alexanderv@bigid.com>
2020-01-27 12:20:58 +0300

    add POST METHOD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,40 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import utils.JSONutil;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Response response = null;
+        RestAssured.baseURI = "http://10.2.0.138:9200";
+        //BufferedWriter writer = new BufferedWriter(new FileWriter("mockData.json"));
+        String json = "";
 
         JSONutil jsoNutil = new JSONutil(5);
-        for (int i = 0; i <= 1000000; i++) {
-            System.out.println(i);
-            System.out.println(jsoNutil.createNewJSON());
+        for (int i = 0; i <= 1; i++) {
+            System.out.println("Number :" + i);
+
+            json = jsoNutil.createNewJSON();
+
+            response = given()
+                   .contentType("application/json")
+                   .header("Authorization", "Basic ZWxhc3RpYzplbGFzdGlj")
+                   .body(json)
+                   .post("/test_index/primary");
+
+            System.out.println("Status Code :" + response.getStatusCode());
+            //writer.write(jsoNutil.createNewJSON());
+            //writer.write("\n");
         }
+
+        //writer.close();
     }
 }
--- src/main/java/utils/JSONutil.java
+++ src/main/java/utils/JSONutil.java
@@ -15,7 +15,7 @@ public class JSONutil {
     }
 
     private String createPair() {
-        String keyValuePair = String.format(" %s : %s ", "_"+mockNeat.strings().size(5).get(), mockNeat.strings().size(15).get() );
+        String keyValuePair = String.format(" %s : %s ", "_"+mockNeat.strings().size(5).get(), mockNeat.strings().get() );
         return keyValuePair;
     }
 
@@ -23,7 +23,7 @@ public class JSONutil {
         StringBuilder stringBuilder = new StringBuilder();
 
         stringBuilder.append(String.format("%s : [", "_"+mockNeat.strings().size(5).get()));
-        int rand = ThreadLocalRandom.current().nextInt(1,10);
+        int rand = ThreadLocalRandom.current().nextInt(1,5);
         for (int i = 0; i <= rand; i++){
             stringBuilder.append(mockNeat.strings().get());
             if (i < rand)
@@ -40,7 +40,7 @@ public class JSONutil {
         stringBuilder.append(String.format("%s : {", "_"+mockNeat.strings().size(5).get()));
         int rand = ThreadLocalRandom.current().nextInt(1,5);
         for (int i = 0; i <= rand; i++){
-            if (i%4 == 0){
+            if (mockNeat.bools().get()){
                 stringBuilder.append(createInsertedMass());
             }
             else{
@@ -49,19 +49,20 @@ public class JSONutil {
             if (i < rand)
                 stringBuilder.append(",");
         }
-        stringBuilder.append("},");
+        stringBuilder.append("}");
 
         return  stringBuilder.toString();
     }
 
-    public String createNewJSON(){
+    public String createNewJSON() {
 
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("{");
 
         for (int i = 0; i <= countOfMainKeys; i++){
-            if (i%4 == 0) {
+            if (mockNeat.bools().get()) {
                 stringBuilder.append(createInsertedObject());
+                stringBuilder.append(",");
             }
             else {
                 stringBuilder.append(createPair());
@@ -75,6 +76,8 @@ public class JSONutil {
         stringBuilder.append(String.format("_email : \"%s\",", mockNeat.emails().get()));
         stringBuilder.append(String.format("_login : \"%s\"", mockNeat.names().first().get().toUpperCase()));
         stringBuilder.append("}");
+
+        //System.out.println(stringBuilder.toString());
 
         JsonObject jsonObject = new JsonParser().parse(stringBuilder.toString()).getAsJsonObject();
         json = stringBuilder.toString();
