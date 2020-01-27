e837b38543ffcbdd2b4144cd9bad30ce74fb1c49
alexandervolkov1211 <alexanderv@bigid.com>
2020-01-27 14:04:21 +0300

    bug-fix with 400 error while pushing more then 32 jsons to one doc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,13 +13,25 @@ import static io.restassured.RestAssured.when;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+
+        int index = 0;
+        int prefix = 1;
+        String doc = "index_" + prefix;
+
         Response response = null;
         RestAssured.baseURI = "http://10.2.0.138:9200";
         //BufferedWriter writer = new BufferedWriter(new FileWriter("mockData.json"));
         String json = "";
 
         JSONutil jsoNutil = new JSONutil(5);
-        for (int i = 0; i <= 1; i++) {
+        for (int i = 0; i <= 10000; i++) {
+
+            if (index == 31){
+                index = 0;
+                ++prefix;
+                doc = "index_" + prefix;
+            }
+
             System.out.println("Number :" + i);
 
             json = jsoNutil.createNewJSON();
@@ -28,9 +40,12 @@ public class Main {
                    .contentType("application/json")
                    .header("Authorization", "Basic ZWxhc3RpYzplbGFzdGlj")
                    .body(json)
-                   .post("/test_index/primary");
+                   .post(String.format("/%s/primary", doc));
 
             System.out.println("Status Code :" + response.getStatusCode());
+            System.out.println("Response as String :" + response.asString());
+
+            ++index;
             //writer.write(jsoNutil.createNewJSON());
             //writer.write("\n");
         }
